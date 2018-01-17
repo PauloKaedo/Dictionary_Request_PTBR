4f0c5d0834a2c61fa0958ff32cfcc0305b631c34
Paulo Henrique <gladgamer01@gmail.com>
2018-01-16 23:17:27 -0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using Newtonsoft.Json;
@@ -16,33 +11,31 @@ namespace Dictionary
     {
         WebClient wc = new WebClient();
         public Main()
-        {
+        {           
             InitializeComponent();
             
-        }
-
-        private void search_Click(object sender, EventArgs e)
-        {
+            search.Click+=delegate
+            {
             try
             {
                 string signJson;
-                string Url = ("http://dicionario-aberto.net/search-json/" + word.Text);
+                string Url = ($"http://dicionario-aberto.net/search-json/{word.Text});
                 signJson = wc.DownloadString(Url);
                 dynamic definition = JsonConvert.DeserializeObject<dynamic>(signJson);
 
-                sign.Text = strToUISO(""+definition.entry.sense[0].def);
+                sign.Text = strToUISO(definition.entry.sense[0].def);
             }
             catch (Exception ex)
             {
-                sign.Text = "Palavra inválida ou não encontrada";
+                sign.Text = ex.ToString();
             }
+            }
+            
         }
-
         private String strToUISO(string str)
         {
             var utf8 = Encoding.Default.GetBytes(str);
-            var ascii = Encoding.UTF8.GetString(utf8);
-            return ""+ascii;
+            return utf8;
         }
     }
 }
